045d0f9f0482e7d867b4eb0ef57938796a2e248a
Ashur Cabrera <ashur@panic.com>
2016-02-26 15:42:55 -0800

    adding Router

--- src/Web/Router.php
+++ src/Web/Router.php
@@ -5,7 +5,46 @@
  */
 namespace Huxtable\Web;
 
+use \Huxtable\Core\FileInfo;
+
 class Router
 {
-	
+	/**
+	 * @param
+	 * @return	void
+	 */
+	public function __construct( FileInfo $dirRoutes )
+	{
+		$this->dirRoutes = $dirRoutes;
+	}
+
+	/**
+	 * @return	array
+	 */
+	public function getRoutes( FileInfo $dirRoutes=null )
+	{
+		$routes = [];
+
+		if( is_null( $dirRoutes ) )
+		{
+			$dirRoutes = $this->dirRoutes;
+		}
+
+		$routeFiles = $dirRoutes->children( ['.DS_Store'] );
+
+		foreach( $routeFiles as $routeFile )
+		{
+			if( $routeFile->isDir() )
+			{
+				// Descent into subfolders
+				$routes = array_merge( $routes, $this->getRoutes( $routeFile ) );
+			}
+			else
+			{
+				$routes[] = $routeFile;
+			}
+		}
+
+		return $routes;
+	}
 }
